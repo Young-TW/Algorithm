bd8686f5bcc168edf8356b1b3ad5c514d07ee0d3
Young-TW <young20050727@gmail.com>
2022-08-12 08:17:42 +0800

    bouble sort pass

--- basic/sort/include/bouble.h
+++ basic/sort/include/bouble.h
@@ -6,12 +6,13 @@ public:
     bouble();
     bouble(int array[10]){
         int reg = 0;
-        for(int i=9;i>0;i--){
-            for (int j=1;j<=i;j++){
-                if (array[j] > array[j-1]){
+        for(int i=0;i<10;i++){ // range
+            for(int j=10;j>=i;j--){
+                if (array[j] > array[j+1]){
                     reg = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = reg;
+                    array[j] = array[j+1];
+                    array[j+1] = reg;
+                    // swap(array[j],array[j+1]);
                 }
             }
         }
